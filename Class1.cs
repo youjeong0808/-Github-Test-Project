9f53f542f22a96cbd4c7799afc6e0ab5cc5f4661
youjeong0808 <baba0849@naver.com>
2023-09-15 10:04:49 +0900

    Maincommit

--- Class1.cs
+++ Class1.cs
@@ -3,6 +3,7 @@
 public class Class1
 {9시 37분에 test에서 수정
  9시 53분에 test에서 수정
+10시 3분에 test에서 수정
 	public Class 
 
 	{
@@ -10,4 +11,5 @@ public class Class1
 
 test용입니다
 test에서 마지막으로 수정
+커밋을했지만 10시에 test에서 수정한 것은 뜨지 않았다.
 }
